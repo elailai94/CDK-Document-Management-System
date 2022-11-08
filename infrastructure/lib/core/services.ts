653c7ad99e99e0fe4f9e516b45eb006aa42f9b63
Elisha Lai <elisha.lai@icloud.com>
2022-11-08 01:14:39 -0500

    feat: add lambda function for notifications service

--- infrastructure/lib/core/services.ts
+++ infrastructure/lib/core/services.ts
@@ -1,7 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as path from "path";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -16,15 +18,15 @@ interface ServicesProps {
 class Services extends Construct {
   public readonly commentsService: NodejsFunction;
   public readonly documentsService: NodejsFunction;
+  public readonly notificationsService: NodejsFunction;
 
   constructor(scope: Construct, id: string, props: ServicesProps) {
     super(scope, id);
 
     // Comments service
-
     this.commentsService = new NodejsServiceFunction(this, "CommentsService", {
       entry: path.join(__dirname, "..", "..", "..", "services", "comments", "index.js"),
-      handler: "handleComments"
+      handler: "handleComments",
     });
 
     this.commentsService.addEnvironment("DYNAMO_DB_TABLE", props.documentsTable.tableName);
@@ -32,7 +34,6 @@ class Services extends Construct {
     props.documentsTable.grantReadWriteData(this.commentsService);
 
     // Documents service
-
     this.documentsService = new NodejsServiceFunction(this, "DocumentsService", {
       entry: path.join(__dirname, "..", "..", "..", "services", "documents", "index.js"),
       handler: "handleDocuments",
@@ -46,7 +47,28 @@ class Services extends Construct {
     props.documentsTable.grantReadWriteData(this.documentsService);
     props.assetBucket.grantRead(this.documentsService);
     props.uploadBucket.grantWrite(this.documentsService);
+
+    // Notifications service
+    this.notificationsService = new NodejsServiceFunction(this, "NotificationsService", {
+      entry: path.join(__dirname, "..", "..", "..", "services", "notifications", "index.js"),
+      handler: "sendNotification",
+    });
+
+    this.notificationsService.addEnvironment("DYNAMO_DB_TABLE", props.documentsTable.tableName);
+    this.notificationsService.addEnvironment(
+      "EMAIL_ADDRESS",
+      ssm.StringParameter.valueForStringParameter(this, "dms-globomantics-email"),
+    );
+
+    this.notificationsService.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["ses:SendEmail"],
+        resources: ["*"],
+      }),
+    );
+
+    props.documentsTable.grantReadData(this.notificationsService);
   }
 }
 
-export { Services};
+export { Services };
---
+++ services/notifications/index.js
@@ -0,0 +1,88 @@
+import { awsClients } from "../common";
+
+// Setup SES client
+const ses = awsClients.ses();
+
+// Get environment variable values
+const emailAddress = process.env.EMAIL_ADDRESS;
+
+async function sendEmail(message, subject) {
+  const params = {
+    Destination: {
+      ToAddresses: [emailAddress],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: message,
+        },
+      },
+      Subject: {
+        Data: `[Globomantics DMS] ${subject}`,
+      },
+    },
+    Source: emailAddress,
+  };
+
+  return ses.sendEmail(params).promise();
+}
+
+async function handleCommentAdded(event) {
+  const { documentId, commentId } = event.detail;
+
+  if (!documentId || !commentId) {
+    // eslint-disable-next-line no-console
+    console.error("Document ID and Comment ID not provided for notification from EventBridge");
+    throw new Error("Could not send notifications for a comment");
+  }
+
+  const message = "Document Comment";
+  const subject = `Comment left on document ${documentId}`;
+
+  try {
+    await sendEmail(message, subject);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Could not send email on CommentAdded event: ${error}`);
+  }
+}
+
+async function handleProcessingFailed(event) {
+  const filename = event.detail?.filename;
+  const owner = event.detail?.owner;
+
+  if (!filename || !owner) {
+    throw new Error("Did not receive filename and or owner with event");
+  }
+
+  const message = `Your file, ${filename}, failed to process. It may be encrypted. Please update your file and try again.`;
+  const subject = "Document Failed Processing";
+
+  try {
+    await sendEmail(message, subject);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Could not send email on FailedProcessing event: ${error}`);
+  }
+}
+
+// Lambda handler
+async function sendNotification(event) {
+  const { source } = event;
+  const detailType = event["detail-type"];
+
+  if (source === "com.globomantics.dms.processing" && detailType === "ProcessingFailed") {
+    return handleProcessingFailed(event);
+  }
+
+  if (source === "com.globomantics.dms.comments" && detailType === "CommentAdded") {
+    return handleCommentAdded(event);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(`Notification event not handled: ${JSON.stringify(event)}`);
+
+  throw new Error("Event not handled");
+}
+
+export { sendNotification };
